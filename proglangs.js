78a4854c69296fecd73a5b383b3717241ae6122a
afykerud <andersfykerud@hotmail.com>
2013-09-19 23:36:49 +0200

    Update proglangs.js
    oppdatert javascript til oppgave 2 lab 2

--- proglangs.js
+++ proglangs.js
@@ -1,9 +1,15 @@
-var element_unordered_list = 
-document.getElementById("proglangs");
+var element_unordered_list = document.getElementById("proglangs");
 alert(element_unordered_list);
+
 
 function printToOutputBox(selectobj) {
 var e_proglangs = document.getElementById("proglangs");
 var e_outputbox = document.getElementById("outputbox");
-e_outputbox.innerHTML = "Du har valgt programmeringsspråket " + e_proglangs. options[selectobj.selectedIndex].text;
+e_outputbox.innerHTML = "Du har valgt programmeringsspråket " + e_proglangs.		// Forandrer innholdet i outputbox
+options[selectobj.selectedIndex].text;
+
 }
+
+function funksjon()
+{
+alert("Beklager så mye, denne siden er under konstruksjon!");				// Gjør at funksjonen sender et varsel.
